c7215b73c7bb0380d333cb17efc801be5fff181e
GodGleb <33755267+GodGleb@users.noreply.github.com>
2017-11-20 18:39:30 +0200

    Update View.cs

--- src/tictactoeV1.0/View/View.cs
+++ src/tictactoeV1.0/View/View.cs
@@ -7,14 +7,15 @@ using System.Threading.Tasks;
 class View
 {
     public ViewDelegate viewDelegate { get; set; }
-    
-    public void print(Value[,] gamefield, Value currentValue) 
+    public void print(Value[,] gamefield, Value currentValue)
     {
-        //вывод игрового поля в консоль и вывод знака (Х\О) текущего игрока
+        Console.WriteLine("                                 __|__|__");//вывод игрового поля в консоль и вывод знака (Х\О) текущего игрока 
+        Console.WriteLine("                                 __|__|__");//в клетки, я так понимаю, надо ссылки повставлять на "пустое значение" "Х" и "0", только я не знаю где эти ссылки брать
+        Console.WriteLine("                                   |  |  ");
     }
-    
     public void gameOver(Value value)
     {
-        //вывод знака (Х\О) победителя
+        Console.WriteLine("...");   //вывод знака (Х\О) победителя, ссылка на победное значние
+        Console.ReadKey();
     }
 }
