dc38b5f53d54d40265594d5dbcfd966783a54c7f
Dasha <zakalata1998@mail.ru>
2017-11-21 00:45:42 +0200

    сделан model

--- src/tictactoeV1.0/Model/Model.cs
+++ src/tictactoeV1.0/Model/Model.cs
@@ -7,24 +7,29 @@ using System.Threading.Tasks;
 
     class Model
     {
-        // этот массив будет являтся предтсавлением игрового поля
-        private Value[,] gamefieldArray;
+        private Value[,] gamefieldArray;  // этот массив будет являтся предтсавлением игрового поля
 
-        public bool isFree(Point position)
-        {
-            //проверка ячейки на пустоту(Value.none)
-            return false;
+        public bool isFree(Point position) //проверка ячейки на пустоту(Value.none)
+       {
+        
+        if (gamefieldArray[position.x, position.y] == Value.none)
+            return true;
+            else return false;
         }
 
-        public void set(Value value, Point position)
+        public void set(Value value, Point position) //установка нового значения на заданную позицию
         {
-            //установка нового значения на заданную позицию
+        gamefieldArray[position.x, position.y] = value;
         }
 
-        public Value[,] gamefield()
+        public Value[,] gamefield(Value[,] gamefield) //вернуть текущее состояние игрового поля
         {
-            //вернуть текущее состояние игрового поля
-            return new Value[,]{};
+        for(int i=0; i<3; i++)
+            for(int j=0;j<3;j++)
+            {
+                gamefield[i, j] = gamefieldArray[i, j];
+            }
+            return gamefield;
         }
     }
 
