1ef7fbb844a7d69703090180a16abdc1cb56f28c
Pelikandr <33723523+Pelikandr@users.noreply.github.com>
2017-11-21 01:15:03 +0200

    подправлены массивы

--- src/tictactoeV1.0/View/View.cs
+++ src/tictactoeV1.0/View/View.cs
@@ -9,13 +9,13 @@ class View
     public ViewDelegate viewDelegate { get; set; }
     public void print(Value[,] gamefield, Value currentValue)
     {
-        Console.WriteLine("                                 _{1}_|_{2}_|_{3}_",gamefield[0][0],gamefield[0][1],gamefield[0][2]);//вывод игрового поля в консоль и вывод знака (Х\О) текущего игрока 
-        Console.WriteLine("                                 _{4}_|_{5}_|_{6}_",gamefield[1][0],gamefield[1][1],gamefield[1][2]);
-        Console.WriteLine("                                  {7} | {8} | {9} ",gamefield[2][0],gamefield[2][1],gamefield[2][2]);
+        Console.WriteLine("                                 _{1}_|_{2}_|_{3}_",gamefield[0,0],gamefield[0,1],gamefield[0,2]);//вывод игрового поля в консоль и вывод знака (Х\О) текущего игрока 
+        Console.WriteLine("                                 _{4}_|_{5}_|_{6}_",gamefield[1,0],gamefield[1,1],gamefield[1,2]);
+        Console.WriteLine("                                  {7} | {8} | {9} ",gamefield[2,0],gamefield[2,1],gamefield[2,2]);
     }
     public void gameOver(Value value)
     {   
-        Console.WriteLine("besiegt:{0}", value);//вывод знака (Х\О) победителя
+        Console.WriteLine("winner is: {0}", value);//вывод знака (Х\О) победителя
         Console.ReadKey();
     }
 }
