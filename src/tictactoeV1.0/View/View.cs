2832284886dad16f481b529ca2be6707f6e41f19
Pelikandr <Pelikandr@users.noreply.github.com>
2017-11-29 12:06:54 +0200

    view изменили табчику

--- src/tictactoeV1.0/View/View.cs
+++ src/tictactoeV1.0/View/View.cs
@@ -9,13 +9,14 @@ class View
     public ViewDelegate viewDelegate { get; set; }
     public void print(Value[,] gamefield, Value currentValue)
     {
-        Console.WriteLine("====================================================");
-        Console.WriteLine("\t{0}\t||\t{1}\t||\t{2}", gamefield[0, 0], gamefield[0, 1], gamefield[0, 2]);//вывод игрового поля в консоль и вывод знака (Х\О) текущего игрока 
-        Console.WriteLine("====================================================");
-        Console.WriteLine("\t{0}\t||\t{1}\t||\t{2}", gamefield[1, 0], gamefield[1, 1], gamefield[1, 2]);
-        Console.WriteLine("====================================================");
-        Console.WriteLine("\t{0}\t||\t{1}\t||\t{2}", gamefield[2, 0], gamefield[2, 1], gamefield[2, 2]);
-        Console.WriteLine("====================================================");
+        Console.WriteLine(" X\t0\t  \t1\t  \t2");
+        Console.WriteLine("Y ====================================================");
+        Console.WriteLine("0 \t{0}\t||\t{1}\t||\t{2}", gamefield[0, 0], gamefield[0, 1], gamefield[0, 2]);//вывод игрового поля в консоль и вывод знака (Х\О) текущего игрока 
+        Console.WriteLine("  ====================================================");
+        Console.WriteLine("1 \t{0}\t||\t{1}\t||\t{2}", gamefield[1, 0], gamefield[1, 1], gamefield[1, 2]);
+        Console.WriteLine("  ====================================================");
+        Console.WriteLine("2 \t{0}\t||\t{1}\t||\t{2}", gamefield[2, 0], gamefield[2, 1], gamefield[2, 2]);
+        Console.WriteLine("  ====================================================");
         Console.WriteLine("{0} turn", currentValue);
         Point position;
         int x = 0;
@@ -29,19 +30,6 @@ class View
 
         position.x = x;
         position.y = y;
-
-        /*string s,sx,sy;
-        Console.WriteLine("kek");
-        s = Console.ReadLine();
-        Console.WriteLine(s);
-        sy = s.Substring(0);
-        sx = s.Substring(s.IndexOf(" "));
-        
-        Console.WriteLine("ahahah {0}",s.IndexOf(" "));
-        x = Convert.ToInt32(sy);
-        Console.WriteLine("aga {0} {1}",x,y);
-        */
-       // Console.Read();
         
         viewDelegate.set(currentValue, position);
 
